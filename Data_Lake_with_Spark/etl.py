e8bba6e232b27e36c581021356e5d8046a614aa6
Ashish Sharma <ashish_shar@yahoo.com>
2020-12-16 15:37:14 -0800

    latest changes

--- Data_Lake_with_Spark/etl.py
+++ Data_Lake_with_Spark/etl.py
@@ -137,10 +137,6 @@ def main():
 	Transform it using Spark into Data Warehouse tables, and 
 	Load them back to S3 in Parquet format
 	"""
-	Read songs and log data from S3, 
-	Transform it using Spark into Data Warehouse tables, and 
-	Load them back to S3 in Parquet format
-	"""
     spark = create_spark_session()
     input_data = "s3a://udacity-dend/"
     output_data = "s3a://udacity-data-lake-w-spark/"
